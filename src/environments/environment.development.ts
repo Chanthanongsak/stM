d609811a3dec21d2176be586653db608ff88051a
khamdenglao22 <khamdenglao22@gmail.com>
2024-03-12 15:57:09 +0700

    logo

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
   // apiUrl: 'http://19.168.1.22:8000/backoffice/api',
-  apiUrl: 'http://141.164.110.54:8000/backoffice/api',
+  // apiUrl: 'http://141.164.110.54:8000/backoffice/api',
+  apiUrl: 'http://localhost:8888/backoffice/api',
   public_dev: '',
   phetsarathUrl: '',
   baseUrl: '',
