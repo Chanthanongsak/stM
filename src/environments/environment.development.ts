5221b27a394cc3933967e1a4691d6f396027b5cb
khamdenglao22 <khamdenglao22@gmail.com>
2024-03-14 16:15:14 +0700

    api dsn

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   // apiUrl: 'http://19.168.1.22:8000/backoffice/api',
   // apiUrl: 'http://141.164.110.54:8000/backoffice/api',
-  apiUrl: 'https://34.92.144.38/bof/api',
+  apiUrl: 'https://www.stmuangthai.com/bof/api',
   public_dev: '',
   phetsarathUrl: '',
   baseUrl: '',
